791f053f4aeaa2365932f46590d092465d097495
alstondsouza23 <alstonpreethidsouza@gmail.com>
2025-08-30 21:24:00 +0530

    Orb Bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,7 @@ function App() {
 
   return (
     <>
-      <div style={{ width: '100%', height: '600px', position: 'relative', background: '#06000F' }}>
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#00ffff"
-          raysSpeed={1.5}
-          lightSpread={0.8}
-          rayLength={1.2}
-          followMouse={true}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={0.05}
-          className="custom-rays"
-        />
-      </div>
-    <div>
-        <ProfileCard
-  name="Javi A. Torres"
-  title="Software Engineer"
-  handle="javicodes"
-  status="Online"
-  contactText="Contact Me"
-  avatarUrl="/path/to/avatar.jpg"
-  showUserInfo={true}
-  enableTilt={true}
-  enableMobileTilt={false}
-  onContactClick={() => console.log('Contact clicked')}
-  />
-</div>
-
+    
     </>
   )
 }
