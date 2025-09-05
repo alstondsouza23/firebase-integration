b2f82dab3e98e0be717a61a5dd6536f7a4d561a2
alstondsouza23 <88037700+alstondsouza23@users.noreply.github.com>
2025-09-06 02:13:42 +0530

    Update Orb.jsx

--- src/components/Backgrounds/Orb.jsx
+++ src/components/Backgrounds/Orb.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { Renderer, Program, Mesh, Triangle, Vec3 } from "ogl";
 import "./Orb.css";
-//or
+//orb
 export default function Orb({
   hue = 0,
   hoverIntensity = 0.2,
